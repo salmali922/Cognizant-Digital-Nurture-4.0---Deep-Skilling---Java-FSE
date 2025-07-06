31d9a6a4c2603a03d1e894cefaa53d0ff1acdcc0
Salmali Samanta <175810448+salmali922@users.noreply.github.com>
2025-07-06 21:45:27 +0530

    Delete Week-3_Spring_Data_JPA_with_Spring_Boot,_Hibernate/Exercise1_Spring_Data_JPA-Quick_Example directory

--- Week-3_Spring_Data_JPA_with_Spring_Boot,_Hibernate/Exercise1_Spring_Data_JPA-Quick_Example/Exercise2_Difference_between_JPA_Hibernate_and_Spring_Data_JPA/Difference_between _Hibernate_and_SpringDataJPA_Code/Hibernate/Code/TestQuery.sql
+++
@@ -1,8 +0,0 @@
-create table Employee(
-id varchar(2) primary key, 
-name varchar(50),
-salary numeric(15)
-);
-show tables
- ;
- select * from Employee;
--- Week-3_Spring_Data_JPA_with_Spring_Boot,_Hibernate/Exercise1_Spring_Data_JPA-Quick_Example/Exercise2_Difference_between_JPA_Hibernate_and_Spring_Data_JPA/Difference_between _Hibernate_and_SpringDataJPA_Code/SpringDataJpa/Code/src/main/java/orm_learn/repository/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.cognizant.orm_learn.repository;
-
-import com.cognizant.orm_learn.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {}
