1be41aeef20b75c504ad1369f0ab8c538fbf4145
Salmali Samanta <175810448+salmali922@users.noreply.github.com>
2025-07-27 21:30:10 +0530

    Add files via upload

---
+++ Week-6_React/Exercise-4 _ReactJS-HOL_Hands-on in this document/Code/blogapp/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Posts from './Posts';
+import './Styles/blogstyle.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Blog App</h1>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
